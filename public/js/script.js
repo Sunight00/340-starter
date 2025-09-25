0f4e1a7a31f658b2be22741cea101f04a7f26710
Sunight00 <ephraim.ewane@gmail.com>
2025-09-26 00:49:15 +0100

    okay

---
+++ public/js/script.js
@@ -0,0 +1,30 @@
+const password = document.getElementById('account_password');
+const repassword = document.getElementById('re_password');
+const message = document.getElementById('message');
+const form = document.getElementById('registration-form');
+const show_hide = document.getElementById('show-hide');
+form.addEventListener('submit', (event) => {
+  if (password.value !== repassword.value) {
+    event.preventDefault();
+    message.style.color = 'red';
+    message.textContent = 'Passwords do not match';
+  } else {
+    message.style.color = 'green';
+    message.textContent = 'Passwords match';
+  }
+});
+
+show_hide.addEventListener('click', () => {
+  if (password.type ===   'password') {
+    password.setAttribute('type', 'text');
+    show_hide.textContent = 'Hide Password';
+    event.preventDefault();
+  }
+  else{
+    password.setAttribute('type', 'password');
+    show_hide.textContent = 'Show Password';
+    event.preventDefault();
+  }})
+
+
+javascript:for(var f=document.forms,i=f.length;i--;)f[i].setAttribute("novalidate",i)
--- server.js
+++ server.js
@@ -10,14 +10,42 @@ const expressLayouts = require("express-ejs-layouts")
 const env = require("dotenv").config()
 const app = express()
 const static = require("./routes/static")
+
+const bodyParser = require("body-parser")
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({ extended: true })) // for parsing application/x-www-form-urlencoded
+
 const baseController = require("./controllers/baseController")
 const inventoryRoute = require("./routes/inventoryRoute")
 const utilities = require("./utilities/")
-
+const session = require("express-session")
+const pool = require('./database/')
+const accountRoute = require("./routes/accountRoute")
 
 /* ***********************
  * View Engine and Templates
  *************************/
+/* ***********************
+ * Middleware
+ * ************************/
+ app.use(session({
+  store: new (require('connect-pg-simple')(session))({
+    createTableIfMissing: true,
+    pool,
+  }),
+  secret: process.env.SESSION_SECRET,
+  resave: true,
+  saveUninitialized: true,
+  name: 'sessionId',
+}))
+// Express Messages Middleware
+app.use(require('connect-flash')())
+app.use(function(req, res, next){
+  res.locals.messages = require('express-messages')(req, res)
+  next()
+})
+
+
 app.set("view engine", "ejs")
 app.use(expressLayouts)
 app.set("layout", "./layouts/layout") // not at views root
@@ -34,6 +62,16 @@ app.get("/", utilities.handleErrors(baseController.buildHome))
 //app.use("/inv", inventoryRoute)
 app.use("/inv/", utilities.handleErrors(inventoryRoute))
 //app.use("/inv/type/", utilities.handleErrors(inventoryRoute))
+
+
+app.use("/account", utilities.handleErrors(accountRoute))
+
+app.use("/account", utilities.handleErrors(accountRoute))
+
+
+
+
+
 
 
 // Error routes for testing
