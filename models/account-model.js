e0c575a88d1e93e44a4910714ce51f6126846697
Sunight00 <ephraim.ewane@gmail.com>
2025-09-26 01:40:09 +0100

    omo

--- models/account-model.js
+++ models/account-model.js
@@ -12,4 +12,18 @@ accountModel.registerAccount= async function (account_firstname, account_lastnam
   }
 }
 
+
+
+/* **********************
+ *   Check for existing email
+ * ********************* */
+accountModel.checkExistingEmail = async function (account_email){
+  try {
+    const sql = "SELECT * FROM account WHERE account_email = $1"
+    const email = await pool.query(sql, [account_email])
+    return email.rowCount
+  } catch (error) {
+    return error.message
+  }
+}
 module.exports = accountModel
